8d9a5b2208420ed17da2a247ae74c211df400d53
mSoheili <majid.soheily@gmail.com>
2021-05-23 08:10:49 +0430

    Add, optimization methods, QP, SR, and TP

---
+++ src/main/scala/org/apache/spark/ml/feature/OptimizationMethods/SR.scala
@@ -0,0 +1,86 @@
+package org.apache.spark.ml.feature.OptimizationMethods
+
+import breeze.linalg.{eigSym, DenseMatrix => BDM, DenseVector => BDV}
+import org.apache.spark.internal.Logging
+
+
+/**
+  * Spectral Relaxation
+ *  https://sites.google.com/site/vinhnguyenx/softwares => SPEC_CMI
+  */
+class SR(H:Array[Array[Double]]) extends Logging {
+
+  def getWeights: Array[Double] = {
+
+    logInfo("---------- Spectral Relaxation Method ---------------")
+
+    val start: Long = System.currentTimeMillis()
+    val nColumns = H.length
+    val A: BDM[Double] = BDM(H: _*)
+    val es = eigSym(A)
+    val lastFeature: Int = nColumns - 1
+    val ev = es.eigenvectors(::, lastFeature) // biggest eigenvector is placed on the end of list
+    val weights = NormalizeVector(ev).toArray
+    logInfo(s"Spectral relaxation method takes ${System.currentTimeMillis() - start} ms")
+
+    weights
+  }
+
+  private def NormalizeVector(vec: BDV[Double]): BDV[Double] = {
+    //val norm = vec.foldLeft(0.0)((b, v) => b + v * v)
+    val norm = math.sqrt(vec.foldLeft(0.0)((b, v) => b + v * v))
+    vec.mapValues(v => math.abs(v / norm))
+  }
+
+}
+
+object SR {
+
+  def apply(H: Array[Array[Double]], isSymmetric: Boolean = true, isNormalized: Boolean = false): SR = {
+    if (isSymmetric && isNormalized)
+      new SR(H)
+    else {
+
+      val sH = if (isSymmetric) H else SymmetricMatrix(H)
+      val nsH = if (isNormalized) sH else NormalizeMatrix(sH)
+      new SR(nsH)
+
+    }
+  }
+
+  private def SymmetricMatrix(mat: => Array[Array[Double]]): Array[Array[Double]] = {
+    val m_t = mat.transpose
+    mat.zip(m_t).map { case (a1, a2) => a1.zip(a2).map { case (v1, v2) => (v1 + v2) / 2.0 } }
+  }
+
+  private def NormalizeMatrix(mat: => Array[Array[Double]]): Array[Array[Double]] = {
+
+    val minValue = mat.map(a => a.min).min
+    val maxValue = mat.map(a => a.max).max
+    val diff = maxValue - minValue
+    mat.map(a => a.map(v => (v - minValue) / diff))
+  }
+
+}
+
+/*
+def multGen[U,V, W](times: Function2[T,U,V], plus: Function2[W,V,W], plusId: W)
+  	(other: Matrix[U]) = {
+    val (rows, columns) = dim
+    val (orows, ocolumns) = other.dim
+    if (columns != orows) throw new Error("Invalid dimensions for Matrix.mult()")
+
+    val result = Matrix[W](rows,ocolumns)
+
+    for (i <- 0 until rows; j <- 0 until ocolumns){
+      var accum = plusId
+      for (k <- 0 until columns){
+        val p = times(this(i,k), other(k,j))
+        accum = plus(accum, p)
+      }
+      result(i)(j) = accum
+    }
+    result
+  }
+
+ */
---
+++ src/main/scala/org/apache/spark/ml/feature/OptimizationMethods/TP.scala
@@ -0,0 +1,90 @@
+package org.apache.spark.ml.feature.OptimizationMethods
+
+import org.apache.spark.internal.Logging
+
+/**
+  * Truncated power method for sparse eigenvalue problem
+  * Xiao-Tong Yuan, Tong Zhang, Truncated Power Method for Sparse Eigenvalue Problems, Technical Report, 2011
+  * The implementation of code can be found in
+  *   1) https://sites.google.com/site/xtyuan1980/publications. (original version)
+  *   2) https://github.com/a736ii/EnhancingTPower
+  *
+  *   @param Q:  it must be a symmetric positive semidefinite matrix
+  *   @param cardinality: it is the number of informative features
+  *   @param maxIteration: max number of iteration (default: 50)
+  *   @param optTol: optimality tolerance (default: 1e-6)
+  */
+class TP(Q:Array[Array[Double]], cardinality:Int, maxIteration:Int = 50, optTol:Double = 1e-6, verbose:Boolean = false) extends Logging {
+
+  def getWeights:Array[Double] = {
+
+    logInfo("---------- Truncated Power Method ---------------")
+
+    val start: Long = System.currentTimeMillis()
+    val idx = Q.zipWithIndex.map { case (vec, d) => vec(d) }.zipWithIndex.sortBy(_._1).reverse.map(_._2)
+    var x0 = Array.fill(Q.length)(0.0)
+
+    idx.take(cardinality).foreach(i => x0(i) = 1)
+    val nx0 = norm(x0)
+    x0 = x0.map(v => v / nx0)
+
+    var x = x0
+    // A * x0
+    var s = Q.map { row => row.zip(x).map(vv => vv._1 * vv._2).sum }
+    // 2 * s
+    var g = s.map(_ * 2)
+    // x' * s
+    var f = x.zip(s).map(vv => vv._1 * vv._2).sum
+
+    x = truncate_operator(g, cardinality)
+    var f_old = f
+    var i = 1
+    while (i < maxIteration) {
+
+      // power step
+      s = Q.map { row => row.zip(x).map(vv => vv._1 * vv._2).sum }
+      g = s.map(_ * 2)
+
+
+      // truncate step
+      x = truncate_operator(g, cardinality)
+      f = x.zip(s).map(vv => vv._1 * vv._2).sum
+
+      //% Output Log
+      if (verbose)
+        logInfo(s"Truncated power iteration: ($i , $f)")
+
+
+      if (math.abs(f - f_old) < optTol) i = maxIteration
+
+      f_old = f
+      i = i + 1
+    }
+    logInfo(s"Truncated power method takes ${System.currentTimeMillis() - start} ms, in $i iterations")
+    x
+  }
+
+  private def norm(vec:Array[Double]):Double = math.sqrt(vec.map(v => v * v).sum)
+  private def truncate_operator(values: Array[Double] , k:Int):Array[Double] = {
+
+    val u = Array.fill[Double](values.length)(0.0)
+    val idx = values.zipWithIndex.sortBy { case (v, _) => math.abs(v) }.map(_._2)
+    val v_restrict = idx.take(k).map(i => values(i))
+    val nv = norm(v_restrict)
+    val nnv = v_restrict.map(_ / nv)
+    (0 until k).foreach(i => u(idx(i)) = nnv(i))
+    u
+  }
+}
+
+object TP {
+  def apply(Q: Array[Array[Double]], cardinality: Int, isSymmetric: Boolean = true): TP = {
+    if (isSymmetric) new TP(Q, cardinality)
+    else new TP(SymmetricMatrix(Q), cardinality)
+  }
+
+  private def SymmetricMatrix(mat: => Array[Array[Double]]): Array[Array[Double]] = {
+    val m_t = mat.transpose
+    mat.zip(m_t).map { case (a1, a2) => a1.zip(a2).map { case (v1, v2) => (v1 + v2) / 2.0 } }
+  }
+}
