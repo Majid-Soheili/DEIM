cfd36b3acf6bfe7e647363559023e84a8cde597b
mSoheili <majid.soheily@gmail.com>
2021-06-06 13:07:52 +0430

    Update, replace private variable to final variable.

--- src/main/scala/org/apache/spark/ml/feature/SDEM.scala
+++ src/main/scala/org/apache/spark/ml/feature/SDEM.scala
@@ -2,7 +2,8 @@ package org.apache.spark.ml.feature
 
 import org.apache.spark.annotation.Experimental
 import org.apache.spark.ml.Model
-import org.apache.spark.ml.param.{DoubleParam, ParamMap, ParamValidators}
+import org.apache.spark.ml.feature.Fusion.RankFusion
+import org.apache.spark.ml.param.{DoubleParam, Param, ParamMap, ParamValidators}
 import org.apache.spark.ml.param.shared.{HasFeaturesCol, HasLabelCol, HasOutputCol}
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.{DataFrame, Dataset}
@@ -18,26 +19,48 @@ final class SDEM private [ml](override val uid: String, private val featuresWeig
 
   //region -------- Parameters ------------------------------------------
 
+  private val validFusionMethods = Array("mean", "min", "median", "geom.mean", "RRA", "stuart", "owa")
+
+  final val firstFusionMethod = new Param[String](this, "fusionMethod",
+    "A fusion method applied in aggregate local feature rankings",
+    ParamValidators.inArray(validFusionMethods))
+
+  final val secondFusionMethod = new Param[String](this, "fusionMethod",
+    "A fusion method applied in aggregate global feature rankings",
+    ParamValidators.inArray(validFusionMethods))
+
   final val selectionThreshold = new DoubleParam(this, "selectionThreshold",
     "Represents a proportion of features to select, it should be bigger than 0.0 and less than or equal to 1.0. It is by default set to 0.10.",
     ParamValidators.inRange(0.0, 1.0, lowerInclusive = false, upperInclusive = true))
-  setDefault(selectionThreshold -> 0.10)
 
   final val firstRiskFusion = new DoubleParam(this, "firstRiskFusion",
     "The risk value for fusion items, it should be bigger than 0.0 and less than or equal to 1.0. It is by default set to 0.95.",
     ParamValidators.inRange(0.0, 1.0, lowerInclusive = false, upperInclusive = true))
-  setDefault(firstRiskFusion -> 0.05)
 
   final val secondRiskFusion = new DoubleParam(this, "secondRiskFusion",
     "The risk value for fusion items, it should be bigger than 0.0 and less than or equal to 1.0. It is by default set to 0.95.",
     ParamValidators.inRange(0.0, 1.0, lowerInclusive = false, upperInclusive = true))
-  setDefault(secondRiskFusion -> 0.95)
+
+
+  setDefault(firstFusionMethod, "min")
+  setDefault(secondFusionMethod, "min")
+  setDefault(selectionThreshold, 0.10)
+  setDefault(firstRiskFusion, 0.05)
+  setDefault(secondRiskFusion, 0.95)
+
+  def getFirstFusionMethod: String = $(firstFusionMethod)
+
+  def getSecondFusionMethod: String = $(secondFusionMethod)
 
   def getSelectionThreshold: Double = $(selectionThreshold)
 
   def getFirstRiskFusion: Double = $(firstRiskFusion)
 
   def getSecondRiskFusion: Double = $(secondRiskFusion)
+
+  def setFirstFusionMethod(value: String): this.type = set(firstFusionMethod, value)
+
+  def setSecondFusionMethod(value: String): this.type = set(secondFusionMethod, value)
 
   def setSelectionThreshold(value: Double): this.type = set(selectionThreshold, value)
 
@@ -46,6 +69,21 @@ final class SDEM private [ml](override val uid: String, private val featuresWeig
   def setSecondRiskFusion(value: Double): this.type = set(secondRiskFusion, value)
 
   //endregion
+
+  def getRank: Array[Int] = {
+
+    val ranks = featuresWeights.map(w => w.map(ww => ww.zipWithIndex.sortBy(_._1).reverse.map(_._2)))
+    val firstFusion = ranks.map {
+      first =>
+        val input = first.map(_.toSeq)
+        val (rank, _) = RankFusion.perform[Int](input, method = this.getFirstFusionMethod, risk = this.getFirstRiskFusion)
+        rank
+    }
+
+    val (result, _) = RankFusion.perform[Int](firstFusion, method = this.getSecondFusionMethod, risk = this.getSecondRiskFusion)
+    result.toArray
+  }
+
 
   //region -------- Overridable methods ---------------------------------
 
