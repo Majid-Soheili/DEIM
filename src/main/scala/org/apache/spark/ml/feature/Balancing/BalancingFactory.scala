d1bbf1679a426b71f747ecbef5c07b510725e818
mSoheili <majid.soheily@gmail.com>
2021-06-18 16:59:42 +0430

    Update

--- src/main/scala/org/apache/spark/ml/feature/Balancing/BalancingFactory.scala
+++ src/main/scala/org/apache/spark/ml/feature/Balancing/BalancingFactory.scala
@@ -18,12 +18,34 @@ trait BalancingFactory extends NeiManager with Logging {
     else Array.empty[Array[Double]]
   }
 
+  def getBalancedDataAndIndex(method: String, data: Array[Array[Double]], NeiNumber: Int = 5, percentage: Int = 500, threshold: Int = 1, seed: Int = 12345): (Array[Array[Double]], Array[Int]) = {
+
+    if (method.toLowerCase() == "smote") {
+      val synthetic = this.getSmote(data, NeiNumber, percentage, threshold, seed)
+      (synthetic, synthetic.indices.toArray)
+    }
+    else if (method.toLowerCase() == "nearmiss1")
+      (data, this.getNearMissIndexes(data, NeiNumber, percentage, threshold, version = 1))
+    else if (method.toLowerCase() == "nearmiss2") {
+      (data, this.getNearMissIndexes(data, NeiNumber, percentage, threshold, version = 2))
+    } else if (method.toLowerCase() == "baggingundersampling") {
+      (data, this.getRandomUnderSamplingIndexes(data, percentage, seed))
+    } else if (method.toLowerCase() == "none")
+      (data, data.indices.toArray)
+    else
+      (Array.empty[Array[Double]], Array.emptyIntArray)
+  }
+
   def getNearMiss(data: Array[Array[Double]], NeiNumber: Int = 5, percentage: Int = 500, threshold: Int = 5, version: Int = 1): Array[Array[Double]] = {
+    val indexes = this.getNearMissIndexes(data, NeiNumber, percentage, threshold, version)
+    indexes.map(data)
+  }
+
+  def getNearMissIndexes(data: Array[Array[Double]], NeiNumber: Int = 5, percentage: Int = 500, threshold: Int = 5, version: Int = 1): Array[Int] = {
 
     val start = System.currentTimeMillis()
     val (minIndex, othIndex) = this.splitIndexes(data.map(_.last))
     val ns = minIndex.length * percentage / 100
-    val nf: Int = data.head.length - 1
     val nominalFeatures = this.getNominalFeatures(data, threshold)
 
     val neighbours = if (version == 1) new NerNeiHeap(NeiNumber) else new FurNeiHeap(NeiNumber)
@@ -43,11 +65,16 @@ trait BalancingFactory extends NeiManager with Logging {
       }
 
     val selected = (distances zip othIndex).sortBy(_._1).take(ns).map(_._2)
-    val indexes = (minIndex ++ selected).sorted
-    val result = indexes.map(data)
-    logInfo(s"NearMiss1 method takes ${System.currentTimeMillis() - start} milliseconds")
 
-    result
+    val indexes = Array.ofDim[Int](minIndex.length + ns)
+    for (i <- indexes.indices)
+      if (i < minIndex.length)
+        indexes(i) = minIndex(i)
+      else
+        indexes(i) = selected(i - minIndex.length)
+
+    logInfo(s"NearMiss1 method takes ${System.currentTimeMillis() - start} milliseconds")
+    indexes.sorted
   }
 
   def getSmote(data: Array[Array[Double]], NeiNumber: Int = 5, percentage: Int = 500, threshold: Int = 5, seed: Int = 12345): Array[Array[Double]] = {
@@ -86,12 +113,23 @@ trait BalancingFactory extends NeiManager with Logging {
 
   def getRandomUnderSampling(data: Array[Array[Double]], percentage: Int = 500, seed: Int = 12345): Array[Array[Double]] = {
 
+    val indexes = this.getRandomUnderSamplingIndexes(data, percentage, seed)
+    indexes.map(data)
+  }
+
+  def getRandomUnderSamplingIndexes(data: Array[Array[Double]], percentage: Int = 500, seed: Int = 12345): Array[Int] = {
+
     val r = new Random(seed)
     val (minIndex, othIndex) = this.splitIndexes(data.map(_.last))
     val ns = percentage / 100 * minIndex.length
-    val selected = Array.tabulate(ns)(_ => othIndex(r.nextInt(othIndex.length)))
-    val indexes = (minIndex ++ selected).sorted
-    indexes.map(data)
+
+    val indexes = Array.ofDim[Int](minIndex.length + ns)
+    for (i <- indexes.indices)
+      if (i < minIndex.length)
+        indexes(i) = minIndex(i)
+      else
+        indexes(i) = othIndex(r.nextInt(othIndex.length))
+    indexes.sorted
   }
 
   def splitIndexes(labels: Array[Double]): (Array[Int], Array[Int]) = {
@@ -116,10 +154,6 @@ trait BalancingFactory extends NeiManager with Logging {
         o = o + 1
       }
     }
-
-    //val minIndex = labels.zipWithIndex.filter(_._1 == mcValue).map(_._2)
-    //val othIndex = labels.zipWithIndex.filterNot(_._1 == mcValue).map(_._2)
-
     logInfo(s"The number of instances belonging to the minority class: ${minIndex.length}")
     logInfo(s"The number of instances belonging to the other class: ${othIndex.length}")
 
