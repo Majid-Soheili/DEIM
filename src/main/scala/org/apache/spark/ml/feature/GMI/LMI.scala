5afa3da80441115a3815a72a4f23904e34399cf4
mSoheili <majid.soheily@gmail.com>
2021-06-18 10:15:03 +0430

    Update

--- src/main/scala/org/apache/spark/ml/feature/GMI/LMI.scala
+++ src/main/scala/org/apache/spark/ml/feature/GMI/LMI.scala
@@ -5,41 +5,63 @@ import org.apache.spark.ml.feature.Utilities.Discretizing
 
 import scala.collection.mutable
 
-class LMI(method: String, data: => Array[Array[Double]], maxVec: Array[Double], minVec: Array[Double], b:Int) extends Serializable with Discretizing with Logging {
+class LMI(method: String, data:Array[Array[Double]], indexes:Array[Int], b:Int) extends Serializable with Discretizing with Logging {
 
-  private final val numFeatures = maxVec.length
+  private final val numFeatures = data.head.length - 1
   private final val numColumns = numFeatures + 1
-  private final val Dsc = super.discEW(data, maxVec, minVec, b).toVector
-  private final val Dst = TransformColumnar(Dsc)
+  private final val Dsct = super.discAndColumnarEW(data, indexes, nb = b)
 
   def getSimilarity: Array[Array[Double]] = method.toUpperCase() match {
-    case "QP" => MI(Dst, b, true)
-    case "SR" | "TP" => CMI(Dst, b)
+    case "QP" => MI(Dsct, b, true)
+    case "SR" | "TP" => CMI(Dsct, b)
   }
 
-  def MI(data: => Array[Array[Byte]], maxBin: Int, normal: Boolean): Array[Array[Double]] = {
+  def MI(data: Array[Array[Byte]], maxBin: Int, normal: Boolean): Array[Array[Double]] = {
 
     val start: Long = System.currentTimeMillis()
     val nColumns = data.length
     val nInstances = data.head.length.toDouble
     val frqMatrix = Array.ofDim[Int](maxBin, maxBin)
     val entropies = mutable.Map[(Int, Int), Double]()
-    var fx = Array.emptyIntArray
-    var fy = Array.emptyIntArray
+
+    var counter = 0
+    val vFrequencies = Array.fill[Int](maxBin)(0)
+    val mFrequencies = Array.fill[Int](maxBin* maxBin)(0)
 
     for (x <- 0 until nColumns; y <- (x + 1) until nColumns) {
 
       for (i <- 0 until maxBin; j <- 0 until maxBin) frqMatrix(i)(j) = 0
       for (i <- 0 until nInstances.toInt) frqMatrix(data(x)(i))(data(y)(i)) += 1
 
-      entropies.put((x, y), Entropy(frqMatrix.flatten, nInstances))
+      counter = 0
+      for (i <- 0 until maxBin; j <- 0 until maxBin) {
+        mFrequencies(counter) = frqMatrix(i)(j)
+        counter = counter + 1
+      }
+      entropies += (x, y) -> Entropy(mFrequencies, nInstances)
+
       if (y - x == 1) {
-        fx = frqMatrix.map(_.sum)
-        entropies.put((x, x), Entropy(fx, nInstances))
-        fy = frqMatrix.transpose.map(_.sum)
-        entropies.put((y, y), Entropy(fy, nInstances))
+
+        counter = 0
+        for (i <- 0 until maxBin) {
+          var s = 0
+          for (j <- 0 until maxBin) s = s + frqMatrix(i)(j)
+          vFrequencies(counter) = s;
+          counter = counter + 1
+        }
+        entropies += (x, x) -> Entropy(vFrequencies, nInstances)
+
+        counter = 0
+        for (j <- 0 until maxBin) {
+          var s = 0
+          for (i <- 0 until maxBin) s = s + frqMatrix(i)(j)
+          vFrequencies(counter) = s;
+          counter = counter + 1
+        }
+        entropies += (y, y) -> Entropy(vFrequencies, nInstances)
       }
     }
+
 
     val miMatrix = Array.fill[Double](nColumns, nColumns)(0.0)
     var v: Double = 0.0
@@ -55,59 +77,97 @@ class LMI(method: String, data: => Array[Array[Double]], maxVec: Array[Double], 
 
     logInfo(s"Computing local mutual information (LMI) takes ${System.currentTimeMillis() - start} milliseconds")
     miMatrix
+
   }
 
-  def CMI(data: => Array[Array[Byte]], maxBin: Int): Array[Array[Double]] = {
+  def CMI(data: Array[Array[Byte]], maxBin: Int): Array[Array[Double]] = {
 
     val start: Long = System.currentTimeMillis()
     val nColumns = data.length
     val nFeatures: Int = nColumns - 1
     val z: Int = nFeatures
+    var counter = 0
     val nInstances = data.head.length.toDouble
 
     val entropies = mutable.Map[(Int, Int, Int), Double]()
     val frqCube = Array.ofDim[Int](maxBin, maxBin, maxBin)
-    var fx = Array.emptyIntArray
-    var fy = Array.emptyIntArray
-    var fz = Array.emptyIntArray
-    var fxy = Array.emptyIntArray
-    var fyz = Array.emptyIntArray
-    var fxz = Array.emptyIntArray
+
+    val vFrequencies = Array.fill[Int](maxBin)(0)
+    val mFrequencies = Array.fill[Int](maxBin* maxBin)(0)
+    val cFrequencies = Array.fill[Int](maxBin * maxBin * maxBin)(0)
 
     logInfo(s"CMI -> nInstances: $nInstances")
     logInfo(s"CMI -> nFeatures: $nFeatures")
 
     for (x <- 0 until nFeatures; y <- (x + 1) until nFeatures) {
 
-      //val t1 = System.currentTimeMillis()
       for (i <- 0 until maxBin; j <- 0 until maxBin; k <- 0 until maxBin) frqCube(i)(j)(k) = 0
-      for (i <- 0 until nInstances.toInt) {
-        frqCube(data(x)(i))(data(y)(i))(data(z)(i)) += 1
+      for (i <- 0 until nInstances.toInt) frqCube(data(x)(i))(data(y)(i))(data(z)(i)) += 1
+
+      counter = 0
+      for (i <- 0 until maxBin; j <- 0 until maxBin; k <- 0 until maxBin) {
+        cFrequencies(counter) = frqCube(i)(j)(k);
+        counter = counter + 1;
       }
+      entropies += (x, y, z) -> Entropy(cFrequencies, nInstances)
 
-      entropies.put((x, y, z), Entropy(frqCube.flatten.flatten, nInstances))
-
-      fxy = frqCube.flatMap(m => m.map(v => v.sum))
-      entropies.put((x, y, y), Entropy(fxy, nInstances))
+      counter = 0
+      for (i <- 0 until maxBin; j <- 0 until maxBin) {
+        var s = 0
+        for (k <- 0 until maxBin) s = s + frqCube(i)(j)(k)
+        mFrequencies(counter) = s
+        counter = counter + 1
+      }
+      entropies += (x, y, y) -> Entropy(mFrequencies, nInstances)
 
       if (y - x == 1) {
 
-        fx = frqCube.map(m => m.flatten.sum)
-        fxz = frqCube.flatMap(m => m.transpose.map(v => v.sum))
-        entropies.put((x, x, x), Entropy(fx, nInstances))
-        entropies.put((x, z, z), Entropy(fxz, nInstances))
+        counter = 0
+        for (i <- 0 until maxBin) {
+          var s = 0
+          for (j <- 0 until maxBin; k <- 0 until maxBin) s = s + frqCube(i)(j)(k)
+          vFrequencies(counter) = s
+          counter = counter + 1
+        }
+        entropies += (x, x, x) -> Entropy(vFrequencies, nInstances)
+
+        counter = 0
+        for (i <- 0 until maxBin; k <- 0 until maxBin) {
+          var s = 0
+          for (j <- 0 until maxBin) s = s + frqCube(i)(j)(k)
+          mFrequencies(counter) = s; counter = counter + 1;
+        }
+        entropies += (x, z, z) -> Entropy(mFrequencies, nInstances)
       }
 
       if (y - x == 1 && nFeatures - y == 1) {
 
-        fy = frqCube.transpose.map(_.flatten.sum)
-        entropies.put((y, y, y), Entropy(fy, nInstances))
-        fz = frqCube.map(_.transpose).transpose.map(_.flatten.sum)
-        entropies.put((z, z, z), Entropy(fz, nInstances))
-        fyz = frqCube.map(_.flatten).transpose.map(_.sum)
-        entropies.put((y, z, z), Entropy(fyz, nInstances))
+        counter = 0
+        for (j <- 0 until maxBin) {
+          var s = 0
+          for (i <- 0 until maxBin; k <- 0 until maxBin) s = s + frqCube(i)(j)(k)
+          vFrequencies(counter) = s; counter = counter + 1;
+        }
+        entropies += (y, y, y) -> Entropy(vFrequencies, nInstances)
+
+        counter = 0
+        for (k <- 0 until maxBin) {
+          var s = 0
+          for (i <- 0 until maxBin; j <- 0 until maxBin) s = s + frqCube(i)(j)(k)
+          vFrequencies(counter) = s; counter = counter + 1;
+        }
+        entropies += (z, z, z) -> Entropy(vFrequencies, nInstances)
+
+
+        counter = 0
+        for (j <- 0 until maxBin; k <- 0 until maxBin) {
+          var s = 0;
+          for (i <- 0 until maxBin) s = s + frqCube(i)(j)(k)
+          mFrequencies(counter) = s; counter = counter + 1;
+        }
+        entropies += (y, z, z) -> Entropy(mFrequencies, nInstances)
       }
-      //logDebug(s"CMI($x, $y) takes ${System.currentTimeMillis() - t1} milliseconds")
+
     }
 
     val cmiMatrix = Array.fill[Double](nFeatures, nFeatures)(0.0)
@@ -124,9 +184,10 @@ class LMI(method: String, data: => Array[Array[Double]], maxVec: Array[Double], 
 
     logInfo(s"Computing local conditional mutual information (LCMI) takes ${System.currentTimeMillis() - start} milliseconds")
     cmiMatrix
+
   }
 
-  private def Entropy(frequencies: => Array[Int], nInstances: Double): Double = {
+  private def Entropy(frequencies: Array[Int], nInstances: Double): Double = {
     val entropy = frequencies.foldLeft(0.0) {
       case (acc, v) =>
         if (v == 0) acc
@@ -136,19 +197,5 @@ class LMI(method: String, data: => Array[Array[Double]], maxVec: Array[Double], 
         }
     } / math.log(2)
     entropy
-  }
-
-  private def TransformColumnar(data: => Vector[Array[Byte]]): Array[Array[Byte]] = {
-
-    val start = System.currentTimeMillis()
-    val numRow = data.size
-    val DSc = Array.ofDim[Byte](numColumns, numRow)
-    for (i <- 0 until numRow) {
-      for (j <- 0 until numColumns) {
-        DSc(j)(i) = data(i)(j)
-      }
-    }
-    logInfo(s"Transform to Columnar takes ${System.currentTimeMillis() - start} milliseconds")
-    DSc
   }
 }
