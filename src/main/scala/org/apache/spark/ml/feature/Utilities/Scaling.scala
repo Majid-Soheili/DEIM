1de7d6f71ef9211c45a58b3d194a4a9db02d6d6b
mSoheili <majid.soheily@gmail.com>
2021-06-18 10:29:50 +0430

    Update

--- src/main/scala/org/apache/spark/ml/feature/Utilities/Scaling.scala
+++ src/main/scala/org/apache/spark/ml/feature/Utilities/Scaling.scala
@@ -8,7 +8,8 @@ import org.apache.spark.sql.Row
  * Local Scaling methods
  */
 trait Scaling extends Logging {
-  def minMaxScaling(data: => Iterator[Row], maxVec: DenseVector, minVec: DenseVector, features: String, label: String): Iterator[Array[Double]] = {
+
+  def minMaxScaling(data: Iterator[Row], maxVec: DenseVector, minVec: DenseVector, features: String, label: String): Iterator[Array[Double]] = {
 
     val start = System.currentTimeMillis()
     val scale = 1.0
@@ -19,31 +20,24 @@ trait Scaling extends Logging {
       // scaleArray(i) == 0 iff i-th col is constant (range == 0)
       if (range != 0) scale / range else 0.0
     }
+
+    def scaledValue(idx:Int, v:Double):Double = {
+      if (v.isNaN) 0
+      else if (scaleArray(idx) != 0)
+        (v - minVec(idx)) * scaleArray(idx)
+      else
+        constantOutput
+    }
+
     val result = data.map {
       row =>
-        val classLabel = row.getAs[Any](label) match {
-          case v: Short => v.toDouble
-          case v: Double => v
-          case v: Int => v.toDouble
-          case v: Byte => v.toDouble
-        } // class label of the current instance
-
-        val values = row.getAs[Any](features) match {
-          case v: DenseVector => v.toArray :+ classLabel
-          case v: SparseVector => v.toArray :+ classLabel
-        }
-
-        var i = 0
-        while (i < numFeatures) {
-          if (!values(i).isNaN) {
-            if (scaleArray(i) != 0) {
-              values(i) = (values(i) - minVec(i)) * scaleArray(i)
-            } else {
-              // scaleArray(i) == 0 means i-th col is constant
-              values(i) = constantOutput
-            }
-          }
-          i += 1
+        val values = Array.ofDim[Double](numFeatures + 1)
+        values(numFeatures) = row.getAs[Double](label)
+        row.getAs[Any](features) match {
+          case vv: DenseVector =>
+            vv.foreachActive { case (i, v) => values(i) = scaledValue(i, v) }
+          case vv: SparseVector =>
+            vv.foreachActive { case (i, v) => values(i) = scaledValue(i, v) }
         }
         values
     }
@@ -52,7 +46,7 @@ trait Scaling extends Logging {
     result
   }
 
-  def minMaxScaling(data: => Array[Array[Double]], maxVec: Array[Double], minVec: Array[Double]): Array[Array[Double]] = {
+  def minMaxScaling(data: Array[Array[Double]], maxVec: Array[Double], minVec: Array[Double]): Array[Array[Double]] = {
 
     val start = System.currentTimeMillis()
     val scale = 1.0
@@ -64,7 +58,7 @@ trait Scaling extends Logging {
       if (range != 0) scale / range else 0.0
     }
 
-    val result = data.map {
+    data.foreach{
       row =>
         var i = 0
         while (i < numFeatures) {
@@ -82,6 +76,6 @@ trait Scaling extends Logging {
     }
 
     logInfo(s"MinMax scaling takes ${System.currentTimeMillis() - start} milliseconds")
-    result
+    data
   }
 }
